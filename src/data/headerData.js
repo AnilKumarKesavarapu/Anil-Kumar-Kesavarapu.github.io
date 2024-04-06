20016593f5390f86b3c2849d6df45260e58940bb
AnilKumarKesavarapu <166235245+AnilKumarKesavarapu@users.noreply.github.com>
2024-04-06 22:43:18 +0530

    Update headerData.js
    title change

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import introImage from '../assets/introPic.jpeg'
 
 export const headerData = {
     name: 'Anil Kumar',
-    title: "Web Developer",
+    title: "Power System & Data Analyst",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
     image: introImage,
     resumePdf: resume
