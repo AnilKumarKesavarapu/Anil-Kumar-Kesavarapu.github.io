dd9c803a4de08fbe91b1fb02ea561acb00d10352
AnilKumarKesavarapu <166235245+AnilKumarKesavarapu@users.noreply.github.com>
2024-04-06 22:49:09 +0530

    initial commit

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import introImage from '../assets/introPic.jpeg'
 export const headerData = {
     name: 'Anil Kumar',
     title: "Power Systems Engineer & Data Analyst",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    desciption:" wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
     image: introImage,
     resumePdf: resume
 }
